25d59309195e7d5fd67d0030ae910c0c2a68448a
Chikacodes <chikaoji58@gmail.com>
2022-09-08 19:37:09 -0700

    4-puts.c :C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main(void): main function controls our programme
+ *
+ * puts(): prints output
+ *
+ * My_string[]: contains a set of string to be printed
+ *
+ *main(void): our programme started here with our declaration
+ * puts(): prints out a swt of string to the standard output
+ *
+ * stdio.h : returns the value of puts()
+ *
+ * Return: returns a 0 value
+ */
+
+int main(void) /* man(void) not returning any value */
+{
+char My_string[] = "Programming is like building a multilingual puzzle";
+puts(My_string);
+return (0);
+}
