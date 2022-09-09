14b977e7cd2754762ad63b912390d9126622e90d
Chikacodes <chikaoji58@gmail.com>
2022-09-09 08:15:29 -0700

    4-puts.c Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - programme starts here
+*
+* Description:prints out a string of character to the stdout
+*
+* Return: returns zero but not negative
+*/
+int main(void)
+{
+	char My_string[] = "programming is like building a multilingual puzzle";
+	puts(My_string);
+
+	return (0);
+}
