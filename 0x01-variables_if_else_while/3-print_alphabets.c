952273ff86c6f03d903be87970f799ffad692ee3
Chikacodes <chikaoji58@gmail.com>
2022-09-10 02:19:15 -0700

    3-print_alphabets.c: a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - outputs upper and lower case letter
+*
+* Description: main prints both upper and lower case alphabe
+* Return: returns zero value always
+*/
+
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 97; alphabet <= 122; alphabet++)
+	{
+	putchar(alphabet);
+	}
+	putchar(10);
+
+	for (alphabet = 65; alphabet <= 90; alphabet++)
+	{
+	putchar(alphabet);
+	}
+
+	return (0);
+}
