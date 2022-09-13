4121878e84a71c8077cd7b2b3c7b77e248413894
Chikacodes <chikaoji58@gmail.com>
2022-09-13 15:50:23 -0700

    0-putchar.c:
    a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - a program that prints _putchar
+*
+* Description:main prints out the strings followed by new
+* line
+* Return:returns zero value(always sucess)
+*/
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	__putchar("c");
+	_putchar("h");
+	_putchar("a");
+	__putchar("r");
+
+	_putchar(10);
+
+	return (0);
+}
